03ca0ac1d1fef28af3522e5a4513098a6262de61
METADIL MÓVEIS - Caio Lesnock <caio@metadil.com.br>
2020-08-13 12:26:36 -0300

    Fix ip of socket and api

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import store from './store'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000'
+  baseURL: 'http://10.0.2.34:3000'
 })
 
 api.interceptors.request.use(request => {
